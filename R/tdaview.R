b31186df1e8ec871d24648d85f9af2c74f8695e1
WalshKieran <k.walsh@student.unsw.edu.au>
2018-08-20 20:31:05 +1000

    Add files via upload

---
+++ R/tdaview.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+tdaview <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'tdaview',
+    x,
+    width = width,
+    height = height,
+    package = 'tdaview',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for tdaview
+#'
+#' Output and render functions for using tdaview within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a tdaview
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name tdaview-shiny
+#'
+#' @export
+tdaviewOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'tdaview', width, height, package = 'tdaview')
+}
+
+#' @rdname tdaview-shiny
+#' @export
+renderTdaview <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, tdaviewOutput, env, quoted = TRUE)
+}
