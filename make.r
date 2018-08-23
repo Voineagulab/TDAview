564217eeb76cda67843c8fa5df8018a16e7eb969
ktaouk1 <ktaouk_97@hotmail.com>
2018-08-23 14:24:14 +1000

    Updated

--- make.r
+++ make.r
@@ -8,3 +8,31 @@ library(tdaview)
 
 #4. Run
 tdaview("hello, world")
+
+# Set working directory
+
+# Install necessary packages
+install.packages("htmlwidgets")
+install.packages("devtools")
+
+# Display graph in browser (OPTIONAL)
+options(viewer = NULL)
+
+# Build library
+devtools::install()
+library(tdaview)
+
+# TEST DATA
+devtools::install_github("paultpearson/TDAmapper")
+library(TDAmapper)
+First.Example.data = data.frame( x=2*cos(0.5*(1:100)), y=sin(1:100) )
+First.Example.dist = dist(First.Example.data)
+First.Example.mapper <- mapper(dist_object = First.Example.dist,
+                               filter_values = First.Example.data$x,
+                               num_intervals = 6,
+                               percent_overlap = 50,
+                               num_bins_when_clustering = 10)
+MapperNodes <- mapperVertices(First.Example.mapper, 1:100 )
+MapperLinks <- mapperEdges(First.Example.mapper)
+
+tdaview(First.Example.mapper)
